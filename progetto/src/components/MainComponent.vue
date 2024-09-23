0baee65bc8292ac808db6d501731675c297d3d01
Denise Genuardi <genuardidenise@gmail.com>
2024-09-23 22:31:29 +0200

    cards

--- progetto/src/components/MainComponent.vue
+++ progetto/src/components/MainComponent.vue
@@ -6,20 +6,22 @@
     <div class="row">
       <!-- Colonna sinistra: Descrizione del webinar -->
       <div class="col-md-8 mb-4 intro">
-        <h1>{{webinarName}}</h1>
-        <h2>Strategie di Marketing Automation con Reply</h2>
-        <p class="lead">Massimizza il Tuo Potenziale</p>
-        <p>
+        <h1 class="titolo" style="font-size: 30px; margin-top: 50px;">WEBINAR</h1>
+        <h2 class="titolo" style="font-size: 40px;"><b>Strategie di Marketing Automation <br/>con Reply</b></h2>
+        <h3 class="titolo" style="font-size: 20px; margin-bottom: 30px;"><b>Massimizza il Tuo Potenziale</b></h3>
+        <p style="margin-bottom: 20px;">
           Unisciti a noi per un webinar esclusivo in cui esploreremo come Reply
           può rivoluzionare le tue strategie di marketing automation. Scoprirai
           come ottimizzare le campagne, aumentare l'engagement e migliorare il
-          ROI attraverso l'uso intelligente di strumenti di automazione. I
+          ROI attraverso l'uso intelligente di strumenti di automazione. 
+        </p>
+        <p style="margin-bottom: 20px;">I
           nostri esperti condivideranno case study reali e best practices,
           offrendoti insight preziosi per implementare soluzioni efficaci nella
-          tua azienda. Non perdere l'occasione di portare il tuo marketing al
-          livello successivo!
-        </p>
-        <div class="justify-content-center">
+          tua azienda. </p>
+          <p style="margin-bottom: 50px;" ><b>Non perdere l'occasione di portare il tuo marketing al
+            livello successivo!</b></p>
+        <div class="justify-content-center"  style="margin-top: 50px;">
         <button class="btn btn-primary btn-lg" 
         style="
         font-family: IBM Plex Sans JP; 
@@ -49,19 +51,26 @@
         <div class="card mb-3" v-for="speaker in speakers" :key="speaker.id">
           <img
             :src="speaker.image"
-            class="img-fluid rounded-start card-img-top"
+            class="card-img-top"
             :alt="speaker.name"
           />
 
           <div class="card-body">
             <h5 class="card-title">{{ speaker.name }}</h5>
             <p class="card-text">{{ speaker.bio }}</p>
-            <p class="card-text">{{ speaker.argument }}</p>
+            <a class="btn btn-primary" data-bs-toggle="collapse" :href="'#collapse' + speaker.id" role="button" aria-expanded="false" :aria-controls="'collapse' + speaker.id"> Argomento 
+            </a>            
+            <div class="collapse" :id="'collapse' + speaker.id">
+              <div class="card body">
+                <p class="card-text card-body">{{ speaker.argument }}</p>
+              </div>
+            </div>
+          </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+
 </template>
 <!-- eslint-disable prettier/prettier -->
 <script>
@@ -104,7 +113,8 @@ export default {
 <!-- eslint-disable prettier/prettier -->
 <style>
 img {
-  margin-bottom: 50px;
+  margin-bottom: 20px;
+  border-radius: 8px;
   width: 100%;
   height: auto;
 }
@@ -117,5 +127,15 @@ img {
   width: 100%;
   height: 650px;
 }
+
+.titolo {
+  font-family: IBM Plex Sans JP; 
+  padding-bottom: 10px;
+  color: black;
+}
+p{
+  margin-bottom: 50px;
+}
+
 
 </style>
